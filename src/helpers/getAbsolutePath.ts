e1f73da41b4e38cac7bef4627cc1561e7b6b4051
Masashi Kawafuji <masashikawafuji@gmail.com>
2022-07-26 02:10:31 +0900

    `getAbsolutePath`の返り値の型を指定

--- src/helpers/getAbsolutePath.ts
+++ src/helpers/getAbsolutePath.ts
@@ -3,7 +3,7 @@ import path from "node:path";
 export default function getAbsolutePath(
   linkFile: string,
   sourceFileDir: string | undefined
-) {
+): string {
   if (!sourceFileDir) throw new Error(`Can't find the source file.`);
   const currentDir = path.dirname(sourceFileDir);
   return path.resolve(currentDir, linkFile);
